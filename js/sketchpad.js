f6fb2e2bf088e9f31a8dd6895114102fccde1e5b
Antti Rastas <antti.rastas@gmail.com>
2017-02-09 17:08:02 +0200

    create 16x16 grid with HTML CSS and Javascript/jQuery

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,7 +1,15 @@
-$(document).ready(function) {
-	$('.container').append("<div></div>");
-	function() {
-		
-		$('.row1').append("<div></div>");
+$(document).ready(function() {
+	/*Create container div */
+	var $container = $("<div></div>").css("float","left");
+	$('body').append($container);
+	/* Define and call function to create 16 columns of divs in one row*/
+	var createDivs = function(sideLength) {	
+		for (x = 0; x < sideLength; x++) {
+			for (y = 0; y < sideLength; y++) {
+				$("<div></div>").addClass("box").appendTo($container);
+			}
+			$("<div></div>").css("clear","both").appendTo($container);
+		}
 	};
-};
+	createDivs(16);
+});
