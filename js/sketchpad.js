aca7c35179e49ebef0bd9406cab2166190c8d9e4
Antti Rastas <antti.rastas@gmail.com>
2017-02-09 19:20:07 +0200

    Add comments, add button and prompt to set new amount of boxes in a grid.

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-	/*Create button and container div */
+	/* Declare global variables */
+	var sideLength = 10;
+	var userInput = 6;
+
+
+	/* Create button above container div */
 	var $button = $("<button type='button'>Click here!</button>");
 	$($button).css("float","left").appendTo('body');
 	$('<div></div>').css("clear","both").appendTo('body');
+
+	
+	/* Create container div*/
 	var $container = $("<div></div>").css("float","left");
-	$('body').append($container);
-	/* Define and call function to create 16 columns of divs in one row*/
+	$("body").append($container);
+
+	/* Define function to create divs inside container*/
 	var createDivs = function(sideLength) {	
 		for (x = 0; x < sideLength; x++) {
 			for (y = 0; y < sideLength; y++) {
@@ -14,8 +23,28 @@ $(document).ready(function() {
 			$("<div></div>").css("clear","both").appendTo($container);
 		}
 	};
-	createDivs(64);
+
+
+	/* Call the function createDivs to create a 16x16 grid upon page loading */
+	createDivs(16);
+
+
+	/* Define function to change divs to yellow when mouse hovers over them */
 	$(".box").mouseover(function() {
 		$(this).css("background-color","yellow");
 	});
+
+
+	/* Define a function to clear the current grid and ask user what
+	size of grid to create next */
+	$($button).click(function() {
+		$($container).empty();
+		userInput = prompt("Please enter the amount of boxes for each side of the grid");
+		createDivs(userInput);
+		/* Define function to change divs to yellow when mouse hovers over them */
+		$(".box").mouseover(function() {
+		$(this).css("background-color","yellow");
+		});
+	});
+
 });
